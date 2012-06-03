350f9534955b7c92f9782f37de0b2137da007d7c
Georgy Bolyuba <bolyuba@gmail.com>
2012-06-03 18:27:45 +0200

    ex11

--- ex11.c
+++ ex11.c
@@ -2,7 +2,34 @@
 
 int main(int argc, char* argv[])
 {
+	int i = 0;
 
-  return 0;
+	for (i = 1; i < argc; i++) {
+		printf("arg %d: %s\n", i, argv[i]);
+	}
 
+	char *regions[] = {
+		"Prague",
+		"Central Bohemian Region",
+		"South Bohemian Region",
+		"Plzeň Region",
+		"Karlovy Vary Region",
+		"Ústí nad Labem Region",
+		"Liberec Region",
+		"Hradec Králové Region",
+		"Pardubice Region",
+		"Olomouc Region",
+		"Moravian-Silesian Region",
+		"South Moravian Region",
+		"Zlín Region",
+		"Vysočina Region"
+	};
+
+	int number_of_regions = 14;
+
+	for (i =0; i < number_of_regions; i++) {
+		printf("region %d: %s\n", i, regions[i]);
+	}
+
+	return 0;
 }
